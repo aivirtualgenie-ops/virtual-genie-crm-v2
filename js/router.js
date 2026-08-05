1c7cf9a30b51f2fcebb6b80981f24fc463627580
aivirtualgenie-ops <aivirtualgenie@gmail.com>
2026-08-05 23:27:56 +0530

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,20 @@
+function router() {
 
+const page = location.hash.replace("#","");
+
+switch(page){
+
+case "companies":
+loadCompanies();
+break;
+
+default:
+loadDashboard();
+
+}
+
+}
+
+window.addEventListener("hashchange", router);
+
+window.addEventListener("load", router);
