7ecf4fae8ed491c0f947c7230e0d978d4bbbd4a8
aivirtualgenie-ops <aivirtualgenie@gmail.com>
2026-08-15 03:55:35 +0530

    Update company.js

--- js/company.js
+++ js/company.js
@@ -1,7 +1,32 @@
 /* =========================================
    COMPANY PAGE
 ========================================= */
+/* =========================================
+   FORMAT DATE & TIME
+========================================= */
 
+function formatDateTime(dateString) {
+
+    if (!dateString) {
+        return "-";
+    }
+
+    const date = new Date(dateString);
+
+    if (isNaN(date.getTime())) {
+        return dateString;
+    }
+
+    return date.toLocaleString("en-IN", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true
+    });
+
+}
 function loadCompany(id) {
 
     const company = getCompany(id);
